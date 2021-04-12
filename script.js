631bbf31d4ac1a0c8799faaff08850bc7517983e
nhrnhr0 <ronionsegal@gmail.com>
2021-04-12 14:38:20 +0300

    t

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     loop: true,
-  
+
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
     },
     slidesPerView: 7,
-  
+    speed: 2500,
+    freeMode: true,
+    autoplay: {
+        delay: 1,
+        disableOnInteraction: false,
+    },
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  
+
     // And if we need scrollbar
     scrollbar: {
-      el: '.swiper-scrollbar',
+        el: '.swiper-scrollbar',
     },
-  });
+});
+document.querySelector('.swiper-container').onmouseover = function () {
+    console.log('hover');
+    (this).swiper.autoplay.stop();
+};
+document.querySelector('.swiper-container').onmouseout = function () {
+    console.log('done');
+    (this).swiper.autoplay.start();
+};
