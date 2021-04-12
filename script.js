65724f513b011f70d78826d6ace3dd36e90a7d49
nhrnhr0 <ronionsegal@gmail.com>
2021-04-12 15:13:34 +0300

    t

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+
+
+$(function(){
+    var $slider = $('.slider');
+    var sizeImage = 200;
+    var items = $slider.children().length;
+    var itemswidth = (items * sizeImage); // 140px width for each client item 
+    $slider.css('width',itemswidth);
+    
+    var rotating = true;
+    var sliderspeed = 0;
+    var seeitems = setInterval(rotateSlider, sliderspeed);
+    
+    $(document).on({
+      mouseenter: function(){
+        rotating = false; // turn off rotation when hovering
+      },
+      mouseleave: function(){
+        rotating = true;
+      }
+    }, '.showcase');
+    
+    function rotateSlider() {
+      if(rotating != false) {
+        var $first = $('.slider .item:first');
+        $first.animate({ 'margin-left': '-'+sizeImage+'px' }, 3000, "linear", function() {
+          $first.remove().css({ 'margin-left': '0px' });
+          $('.slider .item:last').after($first);
+        });
+      }else{
+        $first.stop();
+      }
+    }
+  });
+  
+/*
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     loop: true,
@@ -33,3 +69,4 @@ document.querySelector('.swiper-container').onmouseout = function () {
     console.log('done');
     (this).swiper.autoplay.start();
 };
+*/
